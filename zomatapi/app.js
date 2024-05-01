8762c7865ff71070906bbc4024e90e89f1acf212
Hardik585 <Hardikkaushik83126@gmail.com>
2024-05-01 09:41:08 +0530

    zomata api created for other service

--- zomatapi/app.js
+++ zomatapi/app.js
@@ -38,7 +38,7 @@ app.get('/location', async (req, res) => {
         try {
             const data = await mongoDb.collection('location').find().toArray();
             if (data) {
-                res.status(302).send(data);
+                res.status(200).send(data);
             }
             else {
                 res.status(204).json({ msg: "No Data Found" });
@@ -52,8 +52,45 @@ app.get('/location', async (req, res) => {
     else {
         res.status(401).json({ msg: "Unauthorized" });
     }
-
 })
+
+//get List all restaurant 
+app.get('/restaurant', async (req, res) => {
+    const state_id = Number(req.query.state_id);
+    let query = {};
+    if (state_id) { query = { state_id } };
+    try {
+        const data = await mongoDb.collection('restaurants').find(query).toArray();
+        if (data.length > 0) {
+            res.status(200).send(data);
+        }
+        else {
+            res.status(400).send("Not able to retrieve");
+        }
+    }
+    catch (error) {
+        console.log("Server side error ", error);
+        res.status(500).send('Interval server error');
+    }
+})
+
+//get list of meal type 
+app.get('/meal', async (req, res) => {
+    try {
+        const data = await mongoDb.collection('mealType').find().toArray();
+        if (data.length > 0) {
+            res.status(200).send(data);
+        }
+        else {
+            res.status(400).send("Not able to retrieve");
+        }
+    }
+    catch (error) {
+        console.log('Internal Server Error ', error);
+        res.status(500).send("Server Side something wrong ");
+    }
+})
+
 
 app.listen(port, (err) => {
     mongoCon();
